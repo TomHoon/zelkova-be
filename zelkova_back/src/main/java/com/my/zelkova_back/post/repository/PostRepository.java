2a3cdd65402ce8eabea12e3eecd6d25d9a762302
hyunbi123 <junghyunbi512@naver.com>
2025-06-19 22:23:57 +0900

    Revert "Post, Category, Board Create"
    This reverts commit 9e06a7994045bdaafbde9f28aef44a8e1c3d1aff.

--- zelkova_back/src/main/java/com/my/zelkova_back/post/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.my.zelkova_back.post.repository;
-
-import com.my.zelkova_back.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
