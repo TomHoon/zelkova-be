fbc16412d649d9523c5523e983d79c99f03db3d6
hyunbi123 <junghyunbi512@naver.com>
2025-06-19 22:31:29 +0900

    Revert "Reapply "Post, Category, Board Create""
    This reverts commit bdfe02a86c290fe33ba4bf06dbc8bb91252b0870.

--- zelkova_back/src/main/java/com/my/zelkova_back/category/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.my.zelkova_back.category.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.my.zelkova_back.category.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {}
-
