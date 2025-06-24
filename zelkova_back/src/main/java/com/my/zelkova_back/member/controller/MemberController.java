de062337f0b4d57320fe5ff64fc85db6c38d8f95
dnjswns5013 <dnjswns5013@naver.com>
2025-06-24 12:44:34 +0900

    member Response 수정

--- zelkova_back/src/main/java/com/my/zelkova_back/member/controller/MemberController.java
+++ zelkova_back/src/main/java/com/my/zelkova_back/member/controller/MemberController.java
@@ -1,14 +1,10 @@
 package com.my.zelkova_back.member.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import com.my.zelkova_back.common.response.ApiResponse;
+import com.my.zelkova_back.common.response.ResponseCode;
 import com.my.zelkova_back.member.dto.FindIdRequest;
 import com.my.zelkova_back.member.dto.FindPwRequest;
 import com.my.zelkova_back.member.dto.JoinRequest;
@@ -26,54 +22,74 @@ public class MemberController {
 
 	private final MemberService memberService;
 
-	
 	/**
 	 * [회원가입]
 	 * 회원가입 정보를 받아 새로운 사용자를 등록합니다.
 	 * 
 	 * @param request JoinRequest
-	 * @return ResponseEntity<String>
+	 * @return ResponseEntity<ApiResponse<String>> - 회원가입 완료 메시지
 	 * 
 	 * TODO:
 	 * - 유효성 검증(@Valid) 적용 예정
 	 * - 비밀번호 암호화 여부 확인
 	 */
 	@PostMapping("/join")
-	public ResponseEntity<String> join(@RequestBody JoinRequest request) {
-		return ResponseEntity.ok(memberService.join(request));
+	public ResponseEntity<ApiResponse<String>> join(@RequestBody JoinRequest request) {
+		return ResponseEntity.ok(ApiResponse.success(ResponseCode.SUCCESS, memberService.join(request)));
 	}
 
+	/**
+	 * [로그인]
+	 * 아이디와 비밀번호를 받아 로그인 처리 및 토큰을 반환합니다.
+	 *
+	 * @param request LoginRequest
+	 * @return ResponseEntity<ApiResponse<?>> - 로그인 결과 및 토큰 정보
+	 */
 	@PostMapping("/login")
-	public ResponseEntity<?> login(@RequestBody LoginRequest request) {
-		return ResponseEntity.ok(memberService.login(request));
+	public ResponseEntity<ApiResponse<?>> login(@RequestBody LoginRequest request) {
+		return ResponseEntity.ok(ApiResponse.success(ResponseCode.SUCCESS, memberService.login(request)));
 	}
 
-	
 	/**
 	 * [아이디 찾기]
 	 * 이름, 전화번호, 생년월일을 기반으로 가입된 사용자의 아이디(username)를 반환합니다.
 	 * 
 	 * @param request FindIdRequest - 이름, 전화번호, 생년월일 정보를 포함한 요청 객체
-	 * @return ResponseEntity<String> - 가입된 아이디(username)
+	 * @return ResponseEntity<ApiResponse<String>> - 가입된 아이디(username)
 	 * 
 	 * TODO:
 	 * - 입력값 유효성 검사 고도화 예정
 	 */
 	@PostMapping("/find/id")
-	public ResponseEntity<String> findId(@RequestBody FindIdRequest request) {
-		return ResponseEntity.ok(memberService.findId(request));
+	public ResponseEntity<ApiResponse<String>> findId(@RequestBody FindIdRequest request) {
+		return ResponseEntity.ok(ApiResponse.success(ResponseCode.SUCCESS, memberService.findId(request)));
 	}
 
+	/**
+	 * [비밀번호 재설정 메일 발송]
+	 * 아이디, 이메일 등 본인 확인 정보를 기반으로 비밀번호 재설정 이메일을 발송합니다.
+	 * 
+	 * @param request FindPwRequest
+	 * @return ResponseEntity<ApiResponse<String>> - 이메일 발송 결과 메시지
+	 */
 	@PostMapping("/find/pw")
-	public ResponseEntity<?> findPw(@RequestBody FindPwRequest request) {
+	public ResponseEntity<ApiResponse<String>> findPw(@RequestBody FindPwRequest request) {
 		memberService.sendResetPasswordMail(request);
-		return ResponseEntity.ok("이메일 전송 완료");
+		return ResponseEntity.ok(ApiResponse.success(ResponseCode.SUCCESS, "이메일 전송 완료"));
 	}
 
+	/**
+	 * [회원 탈퇴]
+	 * 로그인된 사용자의 회원 탈퇴를 처리합니다.
+	 * 
+	 * TODO:
+	 * - @AuthenticationPrincipal UserDetails userDetails 추가 예정
+	 * 
+	 * @return ResponseEntity<ApiResponse<String>> - 탈퇴 완료 메시지
+	 */
 	@PutMapping("/withdraw")
-	public ResponseEntity<?> withdraw() {
-		//받는 값에 @AuthenticationPrincipal UserDetails userDetails 추가예정
-		return ResponseEntity.ok("회원 탈퇴 처리 완료");
+	public ResponseEntity<ApiResponse<String>> withdraw() {
+		return ResponseEntity.ok(ApiResponse.success(ResponseCode.SUCCESS, "회원 탈퇴 처리 완료"));
 	}
 
 	/**
@@ -81,21 +97,29 @@ public class MemberController {
 	 * 닉네임을 기준으로 사용자의 프로필 정보를 반환합니다.
 	 * 
 	 * @param nickname 조회할 사용자 닉네임
-	 * @return ResponseEntity<ProfileResponse> - 자기소개, 생년월일 정보 포함
+	 * @return ResponseEntity<ApiResponse<ProfileResponse>> - 자기소개, 생년월일 정보 포함
 	 * 
 	 * TODO:
 	 * - 프로필 사진 기능 확장 예정
 	 */
 	@GetMapping("/profile/{nickname}")
-	public ResponseEntity<ProfileResponse> viewProfile(@PathVariable String nickname) {
-		return ResponseEntity.ok(memberService.getProfileByNickname(nickname));
+	public ResponseEntity<ApiResponse<ProfileResponse>> viewProfile(@PathVariable String nickname) {
+		return ResponseEntity.ok(ApiResponse.success(ResponseCode.SUCCESS, memberService.getProfileByNickname(nickname)));
 	}
 
+	/**
+	 * [프로필 수정]
+	 * 사용자 본인의 프로필 정보를 수정합니다.
+	 * 
+	 * TODO:
+	 * - @AuthenticationPrincipal UserDetails userDetails 추가 예정
+	 * 
+	 * @param request UpdateProfileRequest
+	 * @return ResponseEntity<ApiResponse<String>> - 수정 완료 메시지
+	 */
 	@PutMapping("/profile/edit")
-	public ResponseEntity<?> updateProfile(@RequestBody UpdateProfileRequest request){
-		//받는 값에 @AuthenticationPrincipal UserDetails userDetails 추가예정
+	public ResponseEntity<ApiResponse<String>> updateProfile(@RequestBody UpdateProfileRequest request) {
 		memberService.updateProfile(request);
-		return ResponseEntity.ok("프로필 수정 완료");
+		return ResponseEntity.ok(ApiResponse.success(ResponseCode.SUCCESS, "프로필 수정 완료"));
 	}
 }
-
