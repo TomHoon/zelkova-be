839b89c08cd5c003a50d744bf1e7360fcb506679
dnjswns5013 <dnjswns5013@naver.com>
2025-07-09 16:38:05 +0900

    보드 수정 (#18)

--- zelkova_back/src/main/java/com/my/zelkova_back/board/entity/Board.java
+++ zelkova_back/src/main/java/com/my/zelkova_back/board/entity/Board.java
@@ -1,21 +1,33 @@
 package com.my.zelkova_back.board.entity;
 
+import com.my.zelkova_back.category.entity.Category;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
-@Setter
-@NoArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category categoryId;
+    
     private String name;
 }
